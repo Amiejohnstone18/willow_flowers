ec8f25c81440dba94fb784f6d01686c96df1be9c
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-14 16:35:29 +0000

    Added products app, models and fixtures

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// Nav menu dropdown call
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
