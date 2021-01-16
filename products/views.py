e8cd19d705fef085e79660721b6405b333907bae
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-16 13:57:19 +0000

    Added categories to individual navigation links

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,9 +8,32 @@ def all_products(request):
     """ A view to return the all the flowers, including sorting and searches """
 
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
 
     context = {
         'products': products, 
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to return the individual product details  """
+
+    product = get_object_or_404(Product, id=product_id)
+
+    context = {
+        'product': product, 
+        'current_categories': categories,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
