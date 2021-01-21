9225435b49f54c3307d7eff623e745247720307b
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-21 13:01:04 +0000

    Added stripe elements to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IC2RqE4pW1z533lXcF0guZWxdS41GkI8JyyyObnaZsmKvF7K4yWXi3bjmaNEKn6HPrh1mnuu2YywaJE66yRIQ1G00s7iBos9q',
     }
 
     return render(request, template, context)
