eeb8fb54a7b5dcb0928ea27a252ea1de26ade4d0
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-22 13:46:26 +0000

    Created contact app models

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ContactForm
 from django.core.mail import send_mail
 
+
 def contactview(request):
     """ A view to return the contact page """
     name = ''
@@ -9,28 +10,29 @@ def contactview(request):
     comment = ''
 
 # Code from- http://www.learningaboutelectronics.com/Articles/How-to-create-a-contact-form-for-website-in-Django.php
+
 # A view to return the contact form
 
-    form= ContactForm(request.POST or None)
+    form = ContactForm(request.POST or None)
     if form.is_valid():
-        name= form.cleaned_data.get("name")
-        email= form.cleaned_data.get("email")
-        comment=form.cleaned_data.get("comment")
+        name = form.cleaned_data.get("name")
+        email = form.cleaned_data.get("email")
+        comment = form.cleaned_data.get("comment")
 
         if request.user.is_authenticated():
-            subject= str(request.user) + "'s Comment"
+            subject = str(request.user) + "'s Comment"
         else:
-            subject= "A Visitor's Comment"
+            subject = "A Visitor's Comment"
 
 
-        comment= name + " with the email, " + email + ", sent the following message:\n\n" + comment;
+        comment = name + " with the email, " + email + ", sent the following message:\n\n" + comment;
         send_mail(subject, comment, 'dlhylton@gmail.com', [email])
 
 
-        context= {'form': form}
+        context = {'form': form}
 
         return render(request, 'contact/contact.html', context)
 
     else:
-        context= {'form': form}
+        context = {'form': form}
         return render(request, 'contact/contact.html', context)
