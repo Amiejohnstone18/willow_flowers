05844e081018c1fb57e7c115bcf8dd3203a03927
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-23 12:59:42 +0000

    Updated the checkout webhook handler to save profile information

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -38,6 +39,20 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile info is the user has saved_info
+        profile = None
+        username - intent.metedata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town = shipping_details.address.town
+                profile.default_street_address = shipping_details.address.street_address
+                profile.default_county = shipping_details.address.state
+                Profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -69,6 +84,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name__iexact=shipping_details.name,
+                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
