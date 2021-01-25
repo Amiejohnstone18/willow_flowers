9d499e85e00b827cda1bb38947b5bf8765dc1920
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-25 14:07:51 +0000

    Removed secret key and set debug

--- willow_flowers/settings.py
+++ willow_flowers/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bqc#1-=efko&c#0x4s(d88jcwvl*js-#@7+3otsw3189v9fwh*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['willow-flowers.herokuapp.com', 'localhost']
 
