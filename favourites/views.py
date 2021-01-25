81f566f8c9336a79e32d8996c7b86c559b030391
Amie Johnstone <amiejohnstone18@gmail.com>
2021-01-25 12:01:59 +0000

    Added the favourites input to product description

--- favourites/views.py
+++ favourites/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_favourites(request):
     """ A view to return the favourites """
+    
     return render(request, 'favourites/favourites.html')
+
+
+def add_to_favourites(request, item_id):
+    """ Add item to favourites """
+
+    redirect_url = request.POST.get('redirect')
+    favourites = request.session.get('favourites', {})
+
+    request.session['favourites'] = favourites
+    print(request.session['favourites'])
+    return redirect(redirect_url)
